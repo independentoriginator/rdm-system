bdae2b3c84c25c1d49e8ff2fbb7b27252b117a8a
Rinat Safiullin <independentoriginator@gmail.com>
2022-01-01 11:40:08 +0500

    Change log registration at the Liquibase Hub

---
+++ rdm-database/scripts/liquibase-registerChangeLog.sh
@@ -0,0 +1,1 @@
+liquibase --changeLogFile=../src/main/db.changelog-master.xml --defaults-file=../.settings/liquibase-dev.properties --hubProjectId=5b0738dc-eea4-455a-b7ea-582986442ce5 registerChangeLog
