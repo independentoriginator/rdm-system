d5b1d861f5b0500c757de570e89b8b1fe351cbfe
rsafiullin <independentoriginator@gmail.com>
2022-05-05 23:38:26 +0500

    After refreshing current view dependent views must be invalidated

---
+++ rdm-database/src/main/triggers/tr_meta_view_invalidate_dependent_views.sql
@@ -0,0 +1,7 @@
+drop trigger if exists tr_meta_view_invalidate_dependent_views on meta_view;
+create trigger tr_meta_view_invalidate_dependent_views
+after update 
+on meta_view
+for each row 
+when (old.is_valid = false and new.is_valid = true)
+execute function ${mainSchemaName}.trf_meta_view_after_refresh();
---
+++ rdm-database/src/main/triggers/trf_meta_view_after_refresh.sql
@@ -0,0 +1,41 @@
+create or replace function trf_meta_view_after_refresh()
+returns trigger
+language plpgsql
+as $$
+begin
+	update ${mainSchemaName}.meta_view meta_view
+	set 
+		is_valid = false
+	from 
+		${mainSchemaName}.v_sys_obj_dependency dep
+	where
+		dep.master_cls_name = old.internal_name
+		and dep.master_cls_schema = 
+			coalesce((
+					select 
+						s.internal_name
+					from 
+						${mainSchemaName}.meta_schema s
+					where
+						s.id = old.schema_id						
+				)
+				, '${mainSchemaName}'
+			)		
+		and dep.dependent_cls_name = meta_view.internal_name 
+		and dep.dependent_cls_schema = 
+			coalesce((
+					select 
+						s.internal_name
+					from 
+						${mainSchemaName}.meta_schema s
+					where
+						s.id = meta_view.schema_id						
+				)
+				, '${mainSchemaName}'
+			)
+		and meta_view.is_valid = true
+	;
+
+	return null;
+end
+$$;			
