c365ec9221212d95a1d428555f12ce4cc8c1ec3b
rsafiullin <independentoriginator@gmail.com>
2022-05-05 23:40:45 +0500

    Invalidation of all indirectly dependent views

--- rdm-database/src/main/triggers/trf_entity_invalidate_dependent_views.sql
+++ rdm-database/src/main/triggers/trf_entity_invalidate_dependent_views.sql
@@ -3,16 +3,34 @@ returns trigger
 language plpgsql
 as $$
 begin
+	with recursive dependent as (
+		select distinct
+			dep.dependent_cls_oid as cls_oid
+			, dep.dependent_cls_name as cls_name
+			, dep.dependent_cls_schema as cls_schema
+		from 
+			ng_rdm.v_sys_obj_dependency dep
+		where
+			dep.master_cls_name = TG_TABLE_NAME
+			and dep.master_cls_schema = TG_TABLE_SCHEMA
+		union
+		select distinct
+			dep.dependent_cls_oid as cls_oid
+			, dep.dependent_cls_name as cls_name
+			, dep.dependent_cls_schema as cls_schema
+		from 
+			dependent
+		join ng_rdm.v_sys_obj_dependency dep
+			on dep.master_cls_oid = dependent.cls_oid
+	)
 	update ${mainSchemaName}.meta_view meta_view
 	set 
 		is_valid = false
 	from 
-		${mainSchemaName}.v_sys_obj_dependency dep
+		dependent
 	where
-		dep.master_cls_name = TG_TABLE_NAME
-		and dep.master_cls_schema = TG_TABLE_SCHEMA
-		and dep.dependent_cls_name = meta_view.internal_name 
-		and dep.dependent_cls_schema = 
+		dependent.cls_name = meta_view.internal_name 
+		and dependent.cls_schema = 
 			coalesce((
 					select 
 						s.internal_name
