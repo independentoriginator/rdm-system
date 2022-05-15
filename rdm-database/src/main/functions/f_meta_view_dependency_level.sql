c8447bacedc801c3b13fb5e381ec62849dcfe836
rsafiullin <independentoriginator@gmail.com>
2022-05-15 22:01:00 +0500

    Change to localized dependency logic within the view

--- rdm-database/src/main/functions/f_meta_view_dependency_level.sql
+++ rdm-database/src/main/functions/f_meta_view_dependency_level.sql
@@ -8,23 +8,11 @@ as $function$
 select 
 	coalesce((
 			select 
-				max(${mainSchemaName}.f_meta_view_dependency_level(i_view_oid => t.master_view_oid))
-			from (
-				select distinct
-					master_view.oid as master_view_oid
-				from
-					pg_catalog.pg_class dependent_view
-				join pg_catalog.pg_rewrite pg_rewrite
-					on pg_rewrite.ev_class = dependent_view.oid
-				join pg_catalog.pg_depend pg_depend
-					on pg_depend.objid = pg_rewrite.oid
-				join pg_catalog.pg_class master_view 
-					on master_view.oid = pg_depend.refobjid
-					and master_view.relkind = 'm'::char
-					and master_view.oid <> dependent_view.oid					
-				where
-					dependent_view.oid = i_view_oid
-			) t
+				max(${mainSchemaName}.f_meta_view_dependency_level(i_view_oid => t.master_cls_oid))
+			from
+				${mainSchemaName}.v_sys_obj_dependency t 
+			where 
+				t.dependent_cls_oid = i_view_oid
 		) + 1,
 		0
 	)
