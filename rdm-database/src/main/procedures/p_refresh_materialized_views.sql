4e3e503d307871cd864429627949ec9f2af189c9
rsafiullin <independentoriginator@gmail.com>
2022-06-07 17:27:43 +0500

    Parallelization of procedure calls

--- rdm-database/src/main/procedures/p_refresh_materialized_views.sql
+++ rdm-database/src/main/procedures/p_refresh_materialized_views.sql
@@ -1,11 +1,14 @@
 create or replace procedure p_refresh_materialized_views(
 	i_refresh_all boolean = false
 	, i_schema_name ${mainSchemaName}.meta_schema.internal_name%type = null
+	, i_thread_max_count integer = 10
 )
 language plpgsql
 as $procedure$
 declare 
-	l_view_rec record;
+	l_view_ids ${type.id}[];
+	l_view_names text;
+	l_view_refresh_commands text[];
 	l_start_timestamp timestamp := clock_timestamp();
 	l_timestamp timestamp;
 begin
@@ -18,43 +21,62 @@ begin
 	while true
 	loop
 		select
-			t.*
+			array_agg(t.id)
+			, string_agg(
+				t.schema_name || '.' || t.internal_name
+				, ', '
+			)
+			, array_agg(
+				format(
+					'refresh materialized view %I.%I'
+					, t.schema_name 
+					, t.internal_name
+				)
+			)
 		into 
-			l_view_rec
+			l_view_ids
+			, l_view_names
+			, l_view_refresh_commands
 		from 
 			${mainSchemaName}.v_meta_view t
-		join ${mainSchemaName}.meta_view meta_view 
-			on meta_view.id = t.id
 		where 
 			t.is_valid = false
-			and (t.schema_name = i_schema_name or i_schema_name is null)
 			and t.is_materialized = true
-			and coalesce(meta_view.is_disabled, false) = false
+			and coalesce(t.is_disabled, false) = false
+		group by 
+			dependency_level
 		order by 
 			dependency_level
-		limit 1
-		for update of meta_view
+		;
+
+		perform
+		from 
+			${mainSchemaName}.meta_view 
+		where 
+			id = any(l_view_ids)
+		for update
 		;
 		
-		if l_view_rec is null then
+		if l_view_ids is null then
 			exit;
 		end if;
 		
-   		raise notice 'Refreshing materialized view %.%...', l_view_rec.schema_name, l_view_rec.internal_name;
+   		raise notice 'Refreshing materialized view(s): %...', l_view_names;
    		
    		l_timestamp := clock_timestamp();
    		
-		execute 
-			format(
-				'refresh materialized view %I.%I'
-				, l_view_rec.schema_name 
-				, l_view_rec.internal_name
-			);
+		call ${mainSchemaName}.p_execute_in_parallel(
+			i_commands => l_view_refresh_commands
+			, i_thread_max_count => i_thread_max_count
+		);	
 			
-		update ${mainSchemaName}.meta_view 
-		set is_valid = true
+		update 
+			${mainSchemaName}.meta_view 
+		set 
+			is_valid = true
 			, refresh_time = current_timestamp
-		where id = l_view_rec.id
+		where 
+			id = any(l_view_ids)
 		;
 			
 		commit;
