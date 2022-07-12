31fd32d0ebc37a3d37266f597675b026ac1df5e6
rsafiullin <independentoriginator@gmail.com>
2022-07-12 13:00:06 +0500

    Elimination of endless looping

--- rdm-database/src/main/functions/f_meta_view_dependency_level.sql
+++ rdm-database/src/main/functions/f_meta_view_dependency_level.sql
@@ -5,15 +5,21 @@ returns integer
 language sql
 stable
 as $function$
-select 
-	coalesce((
-			select 
-				max(${mainSchemaName}.f_meta_view_dependency_level(i_view_oid => t.master_obj_id))
-			from
-				${mainSchemaName}.v_sys_obj_dependency t 
-			where 
-				t.dependent_obj_id = i_view_oid
-		) + 1,
-		0
+with 
+	recursive obj(obj_id,n_level) as (
+		select i_view_oid as obj_id, 0::integer as n_level
+		union all
+		select 
+			d.master_obj_id as obj_id, dependent_obj.n_level + 1 as n_level
+		from
+			${mainSchemaName}.v_sys_obj_dependency d 
+		join obj dependent_obj on dependent_obj.obj_id = d.dependent_obj_id 
+		where 
+			d.master_obj_class = 'routine' 
+			or d.master_obj_type in ('v'::"char", 'm'::"char")
 	)
+select 
+	max(n_level)
+from 
+	obj
 $function$;		
