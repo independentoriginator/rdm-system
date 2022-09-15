63f4298621073ce796d788e12311f9a3c0486997
rsafiullin <independentoriginator@gmail.com>
2022-09-15 23:15:27 +0500

    Create non-existent external function

--- rdm-database/src/main/procedures/p_build_target_view.sql
+++ rdm-database/src/main/procedures/p_build_target_view.sql
@@ -35,7 +35,7 @@ begin
 		end if;
 	end if;
 	
-	if not i_view_rec.is_external or i_view_rec.is_routine = false then
+	if not i_view_rec.is_external or i_view_rec.is_routine = false or not coalesce(i_view_rec.is_created, false) then
 		begin
 			execute i_view_rec.query;
 		exception
