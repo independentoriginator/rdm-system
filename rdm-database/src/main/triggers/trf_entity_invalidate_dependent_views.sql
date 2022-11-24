8fb20f458de1624c3988722f442edd96634b5d63
rsafiullin <independentoriginator@gmail.com>
2022-11-24 14:44:22 +0500

    Optimization of access to the heavy view

--- rdm-database/src/main/triggers/trf_entity_invalidate_dependent_views.sql
+++ rdm-database/src/main/triggers/trf_entity_invalidate_dependent_views.sql
@@ -3,27 +3,34 @@ returns trigger
 language plpgsql
 as $$
 begin
-	with recursive dependent as (
-		select distinct
-			dep.dependent_obj_id as cls_oid
-			, dep.dependent_obj_name as cls_name
-			, dep.dependent_obj_schema as cls_schema
-		from 
-			ng_rdm.v_sys_obj_dependency dep
-		where
-			dep.master_obj_name = TG_TABLE_NAME
-			and dep.master_obj_schema = TG_TABLE_SCHEMA
-			and dep.master_obj_class = 'relation'
-		union
-		select distinct
-			dep.dependent_obj_id as cls_oid
-			, dep.dependent_obj_name as cls_name
-			, dep.dependent_obj_schema as cls_schema
-		from 
-			dependent
-		join ng_rdm.v_sys_obj_dependency dep
-			on dep.master_obj_id = dependent.cls_oid
-	)
+	with recursive 
+		v_sys_obj_dependency as (
+			select 
+				*
+			from 
+				ng_rdm.v_sys_obj_dependency
+		)
+		, dependent as (
+			select distinct
+				dep.dependent_obj_id as cls_oid
+				, dep.dependent_obj_name as cls_name
+				, dep.dependent_obj_schema as cls_schema
+			from 
+				v_sys_obj_dependency dep
+			where
+				dep.master_obj_name = TG_TABLE_NAME
+				and dep.master_obj_schema = TG_TABLE_SCHEMA
+				and dep.master_obj_class = 'relation'
+			union
+			select distinct
+				dep.dependent_obj_id as cls_oid
+				, dep.dependent_obj_name as cls_name
+				, dep.dependent_obj_schema as cls_schema
+			from 
+				dependent
+			join v_sys_obj_dependency dep
+				on dep.master_obj_id = dependent.cls_oid
+		)
 	update ${mainSchemaName}.meta_view meta_view
 	set 
 		is_valid = false
