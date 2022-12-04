8f63ce0d65530021fd26e649148525292f4bee75
rsafiullin <independentoriginator@gmail.com>
2022-12-04 16:49:32 +0500

    Move of the locking into a separate query

--- rdm-database/src/main/triggers/trf_entity_invalidate_dependent_views.sql
+++ rdm-database/src/main/triggers/trf_entity_invalidate_dependent_views.sql
@@ -2,6 +2,8 @@ create or replace function trf_entity_invalidate_dependent_views()
 returns trigger
 language plpgsql
 as $$
+declare 
+	l_view_ids ${type.id}[];
 begin
 	with recursive 
 		v_sys_obj_dependency as (
@@ -31,27 +33,30 @@ begin
 			join v_sys_obj_dependency dep
 				on dep.master_obj_id = dependent.cls_oid
 		)
-		, dependent_view as (
-			select 
-				v.id
-			from 
-				${mainSchemaName}.meta_view v
-			join ${mainSchemaName}.meta_schema s
-				on s.id = v.schema_id
-			join dependent
-				on dependent.cls_name = v.internal_name 
-				and dependent.cls_schema = coalesce(s.internal_name, '${mainSchemaName}')
-			where
-				v.is_valid = true
-			for update of v
-		)
-	update ${mainSchemaName}.meta_view meta_view
+	select 
+		array_agg(v.id)
+	into
+		l_view_ids
+	from (
+		select v.id
+		from 
+			${mainSchemaName}.meta_view v
+		join ${mainSchemaName}.meta_schema s
+			on s.id = v.schema_id
+		join dependent
+			on dependent.cls_name = v.internal_name 
+			and dependent.cls_schema = coalesce(s.internal_name, '${mainSchemaName}')
+		where
+			v.is_valid = true
+		for update of v
+	) v
+	;
+
+	update ${mainSchemaName}.meta_view
 	set 
 		is_valid = false
-	from 
-		dependent_view
 	where
-		meta_view.id = dependent_view.id
+		id = any(l_view_ids)
 	;
 	
 	return null;
