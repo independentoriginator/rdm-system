15d32329394e39d86eaacdb2936b2cf407b80bf1
rsafiullin <independentoriginator@gmail.com>
2022-12-30 15:47:45 +0500

    Additional characters from last word

--- rdm-database/src/main/functions/f_abbreviate_name.sql
+++ rdm-database/src/main/functions/f_abbreviate_name.sql
@@ -1,7 +1,14 @@
+drop function if exists ${stagingSchemaName}.f_abbreviate_name(
+	text
+	, boolean
+	, integer
+);
+
 create or replace function ${stagingSchemaName}.f_abbreviate_name(
 	i_name text
 	, i_adjust_to_max_length boolean = false
 	, i_max_length integer = ${stagingSchemaName}.f_system_name_max_length()
+	, i_leave_last_characters integer = 0
 )
 returns text
 language sql
@@ -20,7 +27,15 @@ from (
 	select 
 		t.ordinal_number
 		, string_agg(
-			regexp_replace(t.subname, '[^A-Z]', '', 'g')
+			regexp_replace(t.subname, '[^A-Z/]', '', 'g')
+			|| case 
+				when coalesce(i_leave_last_characters, 0) > 0 then
+					substring(
+						t.subname
+						, '[A-Z]([^A-Z]{' || i_leave_last_characters::text || '})[^A-Z]*$'
+					) 
+				else ''
+			end
 			, '_'
 		) over(
 			order by 
@@ -44,14 +59,19 @@ from (
 		end as target_name
 	from ( 
 		select 
-			name[1] as subname
-			, sub.ordinal_number
-		from 
-			regexp_matches(
-				i_name
-				, '_*([^_]+)'
-				, 'g'
-			) with ordinality as sub(name, ordinal_number) 
+			upper(left(t.subname, 1)) || right(t.subname, -1) as subname 
+			, t.ordinal_number
+		from ( 
+			select 
+				name[1] as subname
+				, sub.ordinal_number
+			from 
+				regexp_matches(
+					i_name
+					, '_*([^_]+)'
+					, 'g'
+				) with ordinality as sub(name, ordinal_number)
+		) t
 	) t
 ) t
 where 	
