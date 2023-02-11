68bf6a34c777416e281a7f6b9bdae6f3f5619702
rsafiullin <independentoriginator@gmail.com>
2023-02-11 15:41:01 +0500

    Fix: using internal dependency table for invalidating dependent views after a view refreshment

--- rdm-database/src/main/triggers/trf_meta_view_after_refresh.sql
+++ rdm-database/src/main/triggers/trf_meta_view_after_refresh.sql
@@ -7,32 +7,10 @@ begin
 	set 
 		is_valid = false
 	from 
-		${mainSchemaName}.v_sys_obj_dependency dep
+		${mainSchemaName}.meta_view_dependency dep
 	where
-		dep.master_obj_name = old.internal_name
-		and dep.master_obj_schema = 
-			coalesce((
-					select 
-						s.internal_name
-					from 
-						${mainSchemaName}.meta_schema s
-					where
-						s.id = old.schema_id						
-				)
-				, '${mainSchemaName}'
-			)		
-		and dep.dependent_obj_name = meta_view.internal_name 
-		and dep.dependent_obj_schema = 
-			coalesce((
-					select 
-						s.internal_name
-					from 
-						${mainSchemaName}.meta_schema s
-					where
-						s.id = meta_view.schema_id						
-				)
-				, '${mainSchemaName}'
-			)
+		dep.master_view_id = old.id
+		and dep.view_id = meta_view.id 
 		and meta_view.is_valid = true
 	;
 
