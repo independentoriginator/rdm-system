a65a1ff38a84599c364b4d1bdcb9f0010d0a6a64
rsafiullin <independentoriginator@gmail.com>
2023-06-14 11:29:56 +0500

    Not all blocking pids are received from the builtin function

--- rdm-database/src/main/views/v_sys_blocked_activity.sql
+++ rdm-database/src/main/views/v_sys_blocked_activity.sql
@@ -17,11 +17,28 @@ from
 	pg_catalog.pg_locks waiting_lock 
 join pg_catalog.pg_stat_activity blocked_activity 
 	on blocked_activity.pid = waiting_lock.pid
-join lateral (
+left join lateral (
 	select unnest(pg_catalog.pg_blocking_pids(waiting_lock.pid))
+	union
+	select distinct
+		blocking_lock.pid
+	from 
+		pg_catalog.pg_locks blocking_lock
+	where 
+		blocking_lock.locktype = waiting_lock.locktype
+		and blocking_lock.database is not distinct from waiting_lock.database	
+		and blocking_lock.relation is not distinct from waiting_lock.relation
+		and blocking_lock.page is not distinct from waiting_lock.page
+		and blocking_lock.tuple is not distinct from waiting_lock.tuple
+		and blocking_lock.virtualxid is not distinct from waiting_lock.virtualxid
+		and blocking_lock.transactionid is not distinct from waiting_lock.transactionid
+		and blocking_lock.classid is not distinct from waiting_lock.classid
+		and blocking_lock.objid is not distinct from waiting_lock.objid
+		and blocking_lock.objsubid is not distinct from waiting_lock.objsubid
+		and blocking_lock.pid != waiting_lock.pid
 ) as blocking_pid(pid)
 	on true
-join pg_catalog.pg_stat_activity blocking_activity 
+left join pg_catalog.pg_stat_activity blocking_activity 
 	on blocking_activity.pid = blocking_pid.pid
 where 
 	not waiting_lock.granted
