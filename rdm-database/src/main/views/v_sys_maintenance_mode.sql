5dc22258e433447c0927c83bb08ace7786d02f98
rsafiullin <independentoriginator@gmail.com>
2023-06-26 22:22:34 +0500

    Maintenance mode status view

---
+++ rdm-database/src/main/views/v_sys_maintenance_mode.sql
@@ -0,0 +1,23 @@
+create or replace view v_sys_maintenance_mode
+as
+select 
+	'Liquibase'::text as process_name
+	, l.lockgranted as lock_time
+	, l.lockedby as locked_by
+from 
+	${mainSchemaName}.databasechangeloglock l 
+where 
+	l.locked 
+	and extract(day from current_timestamp - l.lockgranted) < 1.0
+union all
+select 
+	'Materialized views refreshing'::text as process_name
+	, null as lock_time
+	, null as locked_by
+from 
+	${mainSchemaName}.v_meta_view v
+where 
+	v.is_materialized
+	and not v.is_populated
+	and not v.is_disabled
+limit 1
