3459d49da703936305b7b23251f41a3352b27ab7
rsafiullin <independentoriginator@gmail.com>
2023-06-29 14:34:17 +0500

    User roles view

---
+++ rdm-database/src/main/views/v_sys_user_role.sql
@@ -0,0 +1,14 @@
+create or replace view v_sys_user_role
+as
+select
+	u.rolname as user_name
+	, r.rolname as role_name
+from
+	pg_catalog.pg_roles u
+join pg_catalog.pg_auth_members m
+	on m.member = u.oid
+join pg_roles r 
+	on r.oid = m.roleid
+where 
+	u.rolcanlogin
+	
