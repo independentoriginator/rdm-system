190688708315049bc08f8ebb270e8fb5af6d9d76
rsafiullin <independentoriginator@gmail.com>
2023-12-29 22:47:12 +0500

    Fix to avoid invalid value for parameter "lc_time"

--- rdm-database/src/main/views/v_month_lc_name.sql
+++ rdm-database/src/main/views/v_month_lc_name.sql
@@ -2,7 +2,7 @@ create or replace view v_month_lc_name
 as
 select 
 	l.tag as lang_code
-	, set_config('lc_time', l.tag || '_' || l.default_country, true) as locale
+	, set_config('lc_time', c.collcollate, true) as locale
 	, m as n_month
 	, to_char(to_date(m::text, 'mm'), 'mm') as s_month_mm					
 	, to_char(to_date(m::text, 'mm'), 'tmmon') as s_month_mon
@@ -11,11 +11,15 @@ select
 	, to_char(to_date(m::text, 'mm'), 'tmMonth') as s_month_cap
 	, to_char(to_date(m::text, 'mm'), 'tmMON') as s_month_mon_uc			
 	, to_char(to_date(m::text, 'mm'), 'tmMONTH') as s_month_uc
-from 
-	ng_rdm."language" l
-	, generate_series(1, 12) m
+from
+	pg_database db 
+join pg_catalog.pg_collation c 
+	on c.collencoding = db."encoding" 
+join ng_rdm."language" l
+	on c.collname = l.tag || '_' || l.default_country
+cross join generate_series(1, 12) m
 where 
-	l.default_country is not null
+	db.datname = current_database() 
 ;
 
 comment on view v_month_lc_name is 'Локализованные имена месяцев';
