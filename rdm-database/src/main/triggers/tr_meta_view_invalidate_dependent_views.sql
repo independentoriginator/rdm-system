570b17792ad03e43c3577af57b91acbfabacc5eb
rsafiullin <independentoriginator@gmail.com>
2024-02-04 10:02:58 +0500

    Refactoring of the condition

--- rdm-database/src/main/triggers/tr_meta_view_invalidate_dependent_views.sql
+++ rdm-database/src/main/triggers/tr_meta_view_invalidate_dependent_views.sql
@@ -3,5 +3,5 @@ create trigger tr_meta_view_invalidate_dependent_views
 after update 
 on meta_view
 for each row 
-when ((old.is_valid = false and (new.is_valid = true or new.is_routine = true)) or (old.is_valid = true and new.is_valid = false))
+when (old.is_valid <> new.is_valid or new.is_routine = true)
 execute function ${mainSchemaName}.trf_meta_view_after_refresh();
