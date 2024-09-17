48c10d292a8042a31bd4eedd7674b5430773f8d0
rsafiullin <independentoriginator@gmail.com>
2024-09-17 17:44:27 +0500

    Granting required permissions

--- rdm-database/src/main/views/v_sys_obj.sql
+++ rdm-database/src/main/views/v_sys_obj.sql
@@ -114,3 +114,38 @@ from (
 ;
 
 comment on view v_sys_obj is 'Объект базы данных';
+
+do $$
+declare 
+	l_roles text := (
+		select 
+			string_agg(
+				r.rolname
+				, ', '
+			)			
+		from 
+			pg_catalog.pg_roles r
+		left join information_schema.role_table_grants g
+			on g.grantee = r.rolname
+			and g.privilege_type = 'SELECT'
+			and g.table_name = 'v_sys_obj'
+			and g.table_schema = '${mainSchemaName}'
+		where 
+			r.rolname in (
+				'${mainEndUserRole}'
+				, '${etlUserRole}'
+			)
+	)
+	;
+begin
+	if l_roles is not null then
+		execute 
+			format(
+				'grant select on ${mainSchemaName}.v_sys_obj to %s'
+				, l_roles 
+			)
+		;
+	end if;
+end
+$$
+;
