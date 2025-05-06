86c8683817614f2e014add4d5646fffc851fb4f6
rsafiullin <independentoriginator@gmail.com>
2025-05-06 18:51:26 +0500

    Unique constraints instead unique indices

--- rdm-database/src/main/procedures/p_build_target_index.sql
+++ rdm-database/src/main/procedures/p_build_target_index.sql
@@ -1,12 +1,16 @@
-create or replace procedure p_build_target_index(
-	i_index_rec record
-)
+create or replace procedure 
+	p_build_target_index(
+		i_index_rec record
+	)
 language plpgsql
 as $procedure$
 declare 
-	l_is_target_index_exists bool := i_index_rec.is_target_index_exists;
+	l_is_target_index_exists bool := i_index_rec.is_target_index_exists
+	;
+	l_is_target_constraint_exists bool := i_index_rec.is_target_constraint_exists
+	;
 begin
-	if l_is_target_index_exists = true 
+	if l_is_target_index_exists
 		and (
 			i_index_rec.id is null
 			or (
@@ -16,32 +20,94 @@ begin
 					or i_index_rec.index_columns <> i_index_rec.target_index_columns
 				)
 			)
+			or (
+				i_index_rec.id is not null 
+				and i_index_rec.is_unique
+				and i_index_rec.is_constraint_used
+			)
 		) 
 	then
-		execute format('
-			drop index %I.%I
-			'
-			, i_index_rec.schema_name
-			, i_index_rec.index_name
-		);
-		l_is_target_index_exists = false;
-	end if;
-	
-	if i_index_rec.id is not null and l_is_target_index_exists = false then
-		execute format('
-			create %s index %I on %I.%I (
-				%s
-			)'
-			, case when i_index_rec.is_unique then 'unique' else '' end
-			, i_index_rec.index_name
-			, i_index_rec.schema_name
-			, i_index_rec.meta_type_name 
-			, i_index_rec.index_columns
-		);
-	end if;
-end
-$procedure$;	
+		execute 
+			format('
+				drop index %I.%I
+				'
+				, i_index_rec.schema_name
+				, i_index_rec.index_name
+			)
+		;
+		l_is_target_index_exists = false
+		;
+	end if
+	;
 
-comment on procedure p_build_target_index(
-	record
-) is 'Генерация целевого индекса';
+	if l_is_target_constraint_exists
+		and (
+			i_index_rec.id is null
+			or i_index_rec.is_constraint_used = false
+			or (
+				i_index_rec.id is not null 
+				and (
+					i_index_rec.is_unique <> i_index_rec.is_target_constraint_unique 
+					or i_index_rec.index_columns <> i_index_rec.target_constraint_columns
+					or i_index_rec.is_constraint_deferrable <> i_index_rec.is_target_constraint_deferrable
+				)
+			)
+		) 
+	then
+		execute 
+			format('
+				alter table %I.%I 
+					drop constraint %I
+				'
+				, i_index_rec.schema_name
+				, i_index_rec.meta_type_name 
+				, i_index_rec.constraint_name
+			)
+		;
+		l_is_target_constraint_exists = false
+		;
+	end if
+	;
+
+	if i_index_rec.id is not null then
+		if i_index_rec.is_constraint_used and l_is_target_constraint_exists = false then
+			execute 
+				format('
+					alter table %I.%I 
+						add constraint %I %s(%s)%s
+					'
+					, i_index_rec.schema_name
+					, i_index_rec.meta_type_name 
+					, i_index_rec.constraint_name
+					, case when i_index_rec.is_unique then 'unique ' else '' end
+					, i_index_rec.index_columns
+					, case when i_index_rec.is_constraint_deferrable then ' deferrable initially deferred' else '' end
+				)
+			;
+		elsif l_is_target_index_exists = false then
+			execute 
+				format('
+					create %s index %I on %I.%I (
+						%s
+					)'
+					, case when i_index_rec.is_unique then 'unique' else '' end
+					, i_index_rec.index_name
+					, i_index_rec.schema_name
+					, i_index_rec.meta_type_name 
+					, i_index_rec.index_columns
+				)
+			;
+		end if
+		;
+	end if
+	;
+end
+$procedure$
+;	
+
+comment on procedure 
+	p_build_target_index(
+		record
+	) 
+	is 'Генерация целевого индекса'
+;
--- rdm-database/src/main/views/v_meta_index.sql
+++ rdm-database/src/main/views/v_meta_index.sql
@@ -1,7 +1,7 @@
 create or replace view v_meta_index
 as
-with  
-	recursive type_index as (
+with recursive 
+	type_index as (
 		select 
 			i.id
 	        , t.id as descendant_type_id
@@ -10,6 +10,8 @@ with
 	        , t.is_temporal
 			, i.tag
 			, i.is_unique 
+			, i.is_constraint_used
+			, i.is_constraint_deferrable
 		from 
 			${mainSchemaName}.meta_type t
 		left join ${mainSchemaName}.meta_index i on i.master_id = t.id 
@@ -21,7 +23,9 @@ with
 	        , t.schema_id
 	        , i.is_temporal
 			, i_inherited.tag
-			, i_inherited.is_unique 
+			, i_inherited.is_unique
+			, i_inherited.is_constraint_used
+			, i_inherited.is_constraint_deferrable
 		from (
 			select distinct 
 				descendant_type_id
@@ -43,11 +47,18 @@ with
 			, i.schema_name
 			, i.index_name
 			, i.index_columns
-			, case when target_index.oid is not null then true else false end as is_target_index_exists
-			, i.is_unique
-			, pg_index.indisunique as is_target_index_unique
-			, pg_index_col.index_columns as target_index_columns
 			, i.is_temporal
+			, i.is_unique
+			, i.is_constraint_used
+			, i.is_constraint_deferrable
+			, i.constraint_name
+			, (target_index.oid is not null) as is_target_index_exists
+			, (target_constraint.oid is not null) as is_target_constraint_exists
+			, pg_index.indisunique as is_target_index_unique
+			, (target_constraint.contype = 'u'::"char") as is_target_constraint_unique
+			, target_constraint.condeferrable as is_target_constraint_deferrable
+			, pg_index_col.index_columns as target_index_columns
+			, pg_constraint_col.constraint_columns as target_constraint_columns
 		from (
 			select
 				i.id
@@ -56,20 +67,32 @@ with
 				, t.internal_name as meta_type_name
 				, coalesce(s.internal_name, '${mainSchemaName}') as schema_name
 				, i.is_unique
+				, i.is_constraint_used
+				, i.is_constraint_deferrable
 				, substring(
 					case when i.is_unique then 'u' else '' end
-					|| 'i_' 
+					|| 'i_'
 					|| t.internal_name 
 					|| '$' 
 					|| i.tag
 					, 1
 					, ${mainSchemaName}.f_system_name_max_length()
 				) as index_name
-				, ic.index_columns || 
-				case 
+				, substring(
+					case when i.is_unique then 'u' else '' end
+					|| 'c_'
+					|| t.internal_name 
+					|| '$' 
+					|| i.tag
+					, 1
+					, ${mainSchemaName}.f_system_name_max_length()
+				) as constraint_name
+				, ic.index_columns
+				|| case 
 					when t.is_temporal then 
 						', valid_to'
-					else ''
+					else 
+						''
 				end as index_columns
 			from 
 				type_index i
@@ -122,10 +145,24 @@ with
 			where
 				a.attrelid = pg_index.indrelid
 				and a.attnum = any(pg_index.indkey)
-		) pg_index_col on true
+		) pg_index_col 
+			on true
+		left join pg_catalog.pg_constraint target_constraint
+			on target_constraint.conrelid = target_table.oid
+			and target_constraint.conname = i.constraint_name
+			and target_constraint.contype = 'u'::"char" 
+		left join lateral (
+			select 
+				string_agg(a.attname, ', ' order by array_position(target_constraint.conkey, a.attnum)) as constraint_columns
+			from 
+				pg_catalog.pg_attribute a
+			where
+				a.attrelid = target_constraint.conrelid
+				and a.attnum = any(target_constraint.conkey)
+		) pg_constraint_col 
+			on true
 		where 
 			i.id is not null
-			and i.index_columns is not null
 	)
 	, fk_index as (
 		select 
@@ -156,6 +193,13 @@ select
 	, i.is_unique
 	, i.is_target_index_unique
 	, i.target_index_columns
+	, i.is_constraint_used
+	, i.is_constraint_deferrable
+	, i.constraint_name
+	, i.is_target_constraint_exists
+	, i.is_target_constraint_unique
+	, i.is_target_constraint_deferrable
+	, i.target_constraint_columns
 from 
 	meta_index i
 union all 
@@ -166,10 +210,17 @@ select
 	, n.nspname::varchar as schema_name
 	, target_index.relname as index_name
 	, pg_index_col.index_columns
-	, true as is_target_index_exists
+	, (pg_index.indexrelid is not null) as is_target_index_exists
 	, pg_index.indisunique as is_unique
 	, pg_index.indisunique as is_target_index_unique
 	, pg_index_col.index_columns as target_index_columns
+	, null as is_constraint_used
+	, null as is_constraint_deferrable
+	, null as constraint_name
+	, null as is_target_constraint_exists
+	, null as is_target_constraint_unique
+	, null as is_target_constraint_deferrable
+	, null as target_constraint_columns
 from 
 	${mainSchemaName}.meta_type t
 left join ${mainSchemaName}.meta_schema s
@@ -197,7 +248,8 @@ left join lateral (
 	where
 		a.attrelid = pg_index.indrelid
 		and a.attnum = any(pg_index.indkey)
-) pg_index_col on true
+) pg_index_col 
+	on true
 where 
 	(t.internal_name not like 'meta\_%' or n.nspname <> '${mainSchemaName}') 
 	and not exists (
@@ -207,7 +259,15 @@ where
 			meta_index
 		where 
 			meta_index.master_id = t.id
-			and meta_index.index_name = target_index.relname
+			and ((
+					meta_index.index_name = target_index.relname
+					and meta_index.is_constraint_used = false
+				)
+				or (
+					meta_index.constraint_name = target_index.relname
+					and meta_index.is_constraint_used = true
+				)
+			)			
 	)
 	and not exists (
 		select 
@@ -226,6 +286,74 @@ where
 		where 
 			uc_index.master_id = t.id
 			and uc_index.index_name = target_index.relname
+	)
+union all 
+select
+	null as id
+	, t.id as master_id
+	, t.internal_name as meta_type_name
+	, n.nspname::varchar as schema_name
+	, target_constraint.conname as index_name
+	, pg_constraint_col.constraint_columns as index_columns
+	, null as is_target_index_exists
+	, (target_constraint.contype = 'u'::"char") as is_unique
+	, null as is_target_index_unique
+	, null as target_index_columns
+	, null as is_constraint_used
+	, null as is_constraint_deferrable
+	, target_constraint.conname as constraint_name
+	, (target_constraint.oid is not null) as is_target_constraint_exists
+	, (target_constraint.contype = 'u'::"char") as is_target_constraint_unique
+	, target_constraint.condeferrable as is_target_constraint_deferrable
+	, pg_constraint_col.constraint_columns as target_constraint_columns
+from 
+	${mainSchemaName}.meta_type t
+left join ${mainSchemaName}.meta_schema s
+	on s.id = t.schema_id
+join pg_catalog.pg_namespace n
+	on n.nspname = coalesce(s.internal_name, '${mainSchemaName}')
+join pg_catalog.pg_class target_table 
+	on target_table.relnamespace = n.oid
+	and target_table.relname = t.internal_name
+left join information_schema.table_constraints pk_constraint 
+	on pk_constraint.table_schema = n.nspname
+	and pk_constraint.table_name = t.internal_name
+	and pk_constraint.constraint_type = 'PRIMARY KEY'
+join pg_catalog.pg_constraint target_constraint
+	on target_constraint.conrelid = target_table.oid
+	and target_constraint.contype = 'u'::"char"
+	and (target_constraint.conname <> pk_constraint.constraint_name or pk_constraint.constraint_name is null)
+	and (target_constraint.conname <> 'uc_' || t.internal_name || '$id_valid_to' or t.is_temporal = false) -- implicit index for the unique constraint
+left join lateral (
+	select 
+		string_agg(a.attname, ', ' order by array_position(target_constraint.conkey, a.attnum)) as constraint_columns
+	from 
+		pg_catalog.pg_attribute a
+	where
+		a.attrelid = target_constraint.conrelid
+		and a.attnum = any(target_constraint.conkey)
+) pg_constraint_col 
+	on true
+where 
+	(t.internal_name not like 'meta\_%' or n.nspname <> '${mainSchemaName}') 
+	and not exists (
+		select 
+			1
+		from 
+			meta_index
+		where 
+			meta_index.master_id = t.id
+			and meta_index.constraint_name = target_constraint.conname
+			and meta_index.is_constraint_used
+	)
+	and not exists (
+		select 
+			1
+		from 
+			uc_index
+		where 
+			uc_index.master_id = t.id
+			and uc_index.index_name = target_constraint.conname
 	)
 ;
 
